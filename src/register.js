e38f30188848689932cef392ae40da8d6fa5c42f
Jacob Pawlik <jacob.pawlik@devk.de>
2021-10-26 15:52:22 +0200

    merge with master

--- src/register.js
+++ src/register.js
@@ -1,18 +1,18 @@
 /* eslint-disable  react/jsx-filename-extension */
 
-import React from 'react';
-import { addons, types } from '@storybook/addons';
-import { AddonPanel } from '@storybook/components';
+import React from "react";
+import { addons, types } from "@storybook/addons";
+import { AddonPanel } from "@storybook/components";
 
-import DerKubaCustomEventPanel from './components/custom-event-panel/custom-event-panel';
+import DerKubaCustomEventPanel from "./components/custom-event-panel/custom-event-panel";
 
-const ADDON_ID = 'custom-event';
+const ADDON_ID = "custom-event";
 const PANEL_ID = `${ADDON_ID}/panel`;
 
 addons.register(ADDON_ID, () => {
   addons.add(PANEL_ID, {
     type: types.PANEL,
-    title: 'Custom Events',
+    title: "Custom Events",
     render: ({ active, key }) => (
       <AddonPanel active={active} key={key}>
         <DerKubaCustomEventPanel />
