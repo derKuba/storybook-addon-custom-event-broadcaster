243ddf90570952f50d02bf5923213a3b0212c9fb
Jacob Pawlik <jacob.pawlik@devk.de>
2021-10-27 13:52:04 +0200

    update snapshot tests

--- src/components/custom-event-panel/custom-event-panel.spec.jsx
+++ src/components/custom-event-panel/custom-event-panel.spec.jsx
@@ -1,16 +1,36 @@
 import React from "react";
-import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
-import CustomEventPanel from "./custom-event-panel";
+const mockedModule = ({ children }) => {
+  return <div>{children}</div>;
+};
 
 jest.mock("@storybook/api", () => ({
   __esModule: true,
   useParameter: () => null,
 }));
 
+jest.mock("@storybook/components", () => ({
+  __esModule: true,
+  TableWrapper: ({ children }) => {
+    return <div>{children}</div>;
+  },
+  ResetWrapper: ({ children }) => {
+    return <div>{children}</div>;
+  },
+  Icons: ({ children }) => {
+    return <div>{children}</div>;
+  },
+  Button: ({ onClick, children }) => (
+    <button onClick={onClick}>{children}</button>
+  ),
+}));
+
+import CustomEventPanel from "./custom-event-panel";
+
 describe("CustomEventPanel", () => {
   test("should render", () => {
-    const wrapper = shallow(<CustomEventPanel />);
-    expect(wrapper).toMatchSnapshot();
+    const tree = renderer.create(<CustomEventPanel />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
